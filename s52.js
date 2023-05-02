98dad9ae921534ec420eec0e881f98991409a842
jisucdn <79298333+jisucdn@users.noreply.github.com>
2023-05-02 15:21:00 +0800

    Delete s52.js

--- s52.js
+++
@@ -1,6 +0,0 @@
-(function (){
-                        var id = "2377029035902478992-17838";
-  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
-  (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async src="https://js.effetspositifs.com/o.js"><\/script>');
-})();
