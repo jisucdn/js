a74750c2c89de504e1c62f6c58b543c3cbdbf051
jisucdn <79298333+jisucdn@users.noreply.github.com>
2023-05-02 14:53:01 +0800

    Delete ss51.js

--- ss51.js
+++
@@ -1,6 +0,0 @@
-(function (){
-                        var id = "2074847283501657340-10443";
-  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
-  (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async  src="https://shengshilm-ads.oss-accelerate.aliyuncs.com/o.js"></script>');
-})();
